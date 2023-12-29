995fca7a931930293ee622aba204704272fd1d18
zweix123 <1979803044@qq.com>
2023-12-29 15:32:38 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="dryad",
+    version="1.0.0",
+    description="Description of your library",
+    long_description="Long description of your library",
+    author="zweix123",
+    author_email="1979803044@qq.com",
+    url="https://github.com/zweix123/dryad",
+    packages=[],
+    install_requires=[],
+)
